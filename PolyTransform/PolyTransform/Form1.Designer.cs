108fceab5d584564ffb6d08c821e9c0a70621b49
Nikolaos Soulantikas <n.soulantikas@hotmail.com>
2015-07-21 20:57:33 +0300

    v1
    Version 1
    AVCS to ERP system Transformation

---
+++ PolyTransform/PolyTransform/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace CodeTransform
+{
+    partial class AVCStoERP
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AVCStoERP));
+            this.rich = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.inENC = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.sgENC = new System.Windows.Forms.CheckBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // rich
+            // 
+            this.rich.AcceptsTab = true;
+            this.rich.AutoWordSelection = true;
+            this.rich.Location = new System.Drawing.Point(221, 27);
+            this.rich.Name = "rich";
+            this.rich.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedVertical;
+            this.rich.Size = new System.Drawing.Size(209, 151);
+            this.rich.TabIndex = 3;
+            this.rich.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(26, 27);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(176, 43);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Load Text File";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(26, 90);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(85, 41);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "3-Months";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(117, 90);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(85, 41);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "6-Months";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(26, 137);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(85, 41);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "9-Months";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(117, 137);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(85, 41);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "12-Months";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // inENC
+            // 
+            this.inENC.AutoSize = true;
+            this.inENC.Location = new System.Drawing.Point(26, 242);
+            this.inENC.Name = "inENC";
+            this.inENC.Size = new System.Drawing.Size(144, 17);
+            this.inENC.TabIndex = 9;
+            this.inENC.Text = "Indian ENC to 12 months";
+            this.inENC.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(365, 259);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // sgENC
+            // 
+            this.sgENC.AutoSize = true;
+            this.sgENC.Location = new System.Drawing.Point(26, 265);
+            this.sgENC.Name = "sgENC";
+            this.sgENC.Size = new System.Drawing.Size(143, 17);
+            this.sgENC.TabIndex = 11;
+            this.sgENC.Text = "Spore ENC to 12 months";
+            this.sgENC.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem1,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(452, 24);
+            this.menuStrip1.TabIndex = 12;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem1
+            // 
+            this.helpToolStripMenuItem1.Name = "helpToolStripMenuItem1";
+            this.helpToolStripMenuItem1.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem1.Text = "Help";
+            this.helpToolStripMenuItem1.Click += new System.EventHandler(this.helpToolStripMenuItem1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(452, 295);
+            this.Controls.Add(this.sgENC);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.inENC);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.rich);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AVCS to ERP";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rich;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.CheckBox inENC;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox sgENC;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem1;
+    }
+}
+
---
+++ PolyTransform/PolyTransform/Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CodeTransform
+{
+    public partial class AVCStoERP : Form
+    {
+        public AVCStoERP()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            inENC.Checked = true;
+            sgENC.Checked = true;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string path = "./file.txt";
+            string message = String.Join(Environment.NewLine, File.ReadLines(path));
+            rich.Text = message;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog openFile1 = new OpenFileDialog();
+            openFile1.Filter = "Text Files|*.txt";
+
+            // If the user selected a file, load its contents into the RichTextBox. 
+            if (openFile1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                rich.LoadFile(openFile1.FileName, RichTextBoxStreamType.PlainText);
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string[] lines = rich.Lines;
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (inENC.Checked)
+                {
+                    if (lines[i].Contains("IN"))
+                    {
+                        lines[i] = "AV" + lines[i] + "A";
+                    }
+                    else
+                    {
+                        if (sgENC.Checked)
+                        {
+                            if (lines[i].Contains("SG"))
+                            {
+                                lines[i] = "AV" + lines[i] + "A";
+                            }
+                            else
+                            {
+                                lines[i] = "AV" + lines[i] + "D";
+                            }
+                        }
+                        else
+                        {
+                            lines[i] = "AV" + lines[i] + "D";
+                        }
+                    }
+                }
+                else if (sgENC.Checked)
+                    if (lines[i].Contains("SG"))
+                    {
+                        lines[i] = "AV" + lines[i] + "A";
+                    }
+                    else
+                    {
+                        lines[i] = "AV" + lines[i] + "D";
+                    }
+
+                else
+                {
+                    lines[i] = "AV" + lines[i] + "D";
+                }
+            }
+            rich.Lines = lines;
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            {
+                string[] lines = rich.Lines;
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    if (inENC.Checked)
+                    {
+                        if (lines[i].Contains("IN"))
+                        {
+                            lines[i] = "AV" + lines[i] + "A";
+                        }
+                        else
+                        {
+                            if (sgENC.Checked)
+                            {
+                                if (lines[i].Contains("SG"))
+                                {
+                                    lines[i] = "AV" + lines[i] + "A";
+                                }
+                                else
+                                {
+                                    lines[i] = "AV" + lines[i] + "C";
+                                }
+                            }
+                            else
+                            {
+                                lines[i] = "AV" + lines[i] + "C";
+                            }
+                        }
+                    }
+                    else if (sgENC.Checked)
+                        if (lines[i].Contains("SG"))
+                        {
+                            lines[i] = "AV" + lines[i] + "A";
+                        }
+                        else
+                        {
+                            lines[i] = "AV" + lines[i] + "C";
+                        }
+
+                    else
+                    {
+                        lines[i] = "AV" + lines[i] + "C";
+                    }
+                }
+                rich.Lines = lines;
+            }
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            {
+                string[] lines = rich.Lines;
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    if (inENC.Checked)
+                    {
+                        if (lines[i].Contains("IN"))
+                        {
+                            lines[i] = "AV" + lines[i] + "A";
+                        }
+                        else
+                        {
+                            if (sgENC.Checked)
+                            {
+                                if (lines[i].Contains("SG"))
+                                {
+                                    lines[i] = "AV" + lines[i] + "A";
+                                }
+                                else
+                                {
+                                    lines[i] = "AV" + lines[i] + "B";
+                                }
+                            }
+                            else
+                            {
+                                lines[i] = "AV" + lines[i] + "B";
+                            }
+                        }
+                    }
+                    else if (sgENC.Checked)
+                        if (lines[i].Contains("SG"))
+                        {
+                            lines[i] = "AV" + lines[i] + "A";
+                        }
+                        else
+                        {
+                            lines[i] = "AV" + lines[i] + "B";
+                        }
+
+                    else
+                    {
+                        lines[i] = "AV" + lines[i] + "B";
+                    }
+                }
+                rich.Lines = lines;
+            }
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+            {
+                string[] lines = rich.Lines;
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    if (inENC.Checked)
+                    {
+                        if (lines[i].Contains("IN"))
+                        {
+                            lines[i] = "AV" + lines[i] + "A";
+                        }
+                        else
+                        {
+                            if (sgENC.Checked)
+                            {
+                                if (lines[i].Contains("SG"))
+                                {
+                                    lines[i] = "AV" + lines[i] + "A";
+                                }
+                                else
+                                {
+                                    lines[i] = "AV" + lines[i] + "A";
+                                }
+                            }
+                            else
+                            {
+                                lines[i] = "AV" + lines[i] + "A";
+                            }
+                        }
+                    }
+                    else if (sgENC.Checked)
+                        if (lines[i].Contains("SG"))
+                        {
+                            lines[i] = "AV" + lines[i] + "A";
+                        }
+                        else
+                        {
+                            lines[i] = "AV" + lines[i] + "A";
+                        }
+
+                    else
+                    {
+                        lines[i] = "AV" + lines[i] + "A";
+                    }
+                }
+                rich.Lines = lines;
+            }
+        }
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            rich.Text = "";
+        }
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Transformation of AVCS codes to ERP codes\nVersion 1.0\n\n\nIdea & Development by:\nNikolaos Soulantikas\nn.soulantikas@hotmail.com", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        private void helpToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("1. Create a simple text file, with the AVCS codes row by row\n2. Click to Load the Text file\n3. Choose the duration of the AVCS\n4. Choose whether the Indian or the Singapore charts have an annual duration", "Help", MessageBoxButtons.OK, MessageBoxIcon.Question);
+        }
+    }
+}
+
